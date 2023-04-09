76bc2c124b5ecf3fd7d7e16f933f725a23af5109
Shan Joel <shan.joeld@gmail.com>
2023-04-09 20:56:25 +0530

    initial commit

---
+++ src/components/NewTodoForm.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const NewTodoForm = ({ onSubmit }) => {
+   const [newItem, setNewItem] = useState('');
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (newItem === '') return;
+
+      onSubmit(newItem);
+
+      setNewItem('');
+   };
+
+   return (
+      <form onSubmit={handleSubmit} className="new-item-form">
+         <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input type="text" id="item" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+         </div>
+         <button className="btn">Add</button>
+      </form>
+   );
+};
+
+export default NewTodoForm;
